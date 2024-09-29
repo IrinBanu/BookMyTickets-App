1eea293309dde1b7c745c9c67dd03b3db550b8c1
Shakira Banu <maryamirin2022@gmail.com>
2024-09-29 19:10:18 +0530

    login updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
 
+email!:string;
+password!:string;
+
+constructor(){
+  console.log('Login component called');
+}
+
+onSubmit(){
+  alert(this.email);
+
+  console.log('form submitted');
+  console.log('Email:', this.email, 'Password:',this.password);
+
+if (this.email=="maryamirin2022@gmail.com" && this.password=="password") {
+
+  alert("loggedin successfully");
+  localStorage.setItem("Logged_In_User", "true");
+  localStorage.setItem("Email:", this.email);
+
+  window.location.href="/movies";
+}
+
+
+else{
+  alert("Invalid Login Credentials");
+}  
+
+}
 }
